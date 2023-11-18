5c34b61b638140553fd1d77f0eb253ee825d4be2
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-18 21:30:23 +0200

    First Commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../Components/Navigation/NavBar'
+
+export default function Home
+() {
+  return (
+    <div>
+       
+       <NavBar/>
+
+
+    </div>
+
+    
+  )
+}
