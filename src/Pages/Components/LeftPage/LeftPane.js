2d622b0ebf2f63103c0f61823fac1133075261b4
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-20 00:57:38 +0200

    Second Commit

---
+++ src/Pages/Components/LeftPage/LeftPane.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import "./leftPane.css"
+import TextsmsIcon from '@mui/icons-material/Textsms';
+import GroupsIcon from '@mui/icons-material/Groups';
+import FeedIcon from '@mui/icons-material/Feed';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import BuildCircleIcon from '@mui/icons-material/BuildCircle';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+export default function LeftPane() {
+  return (
+    <div className="leftPaneBox">
+        <div className="leftPaneContainer">
+            <div className="leftPaneMenu">
+                <li className="leftPaneMenuItem">
+                    <TextsmsIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Messages</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <GroupsIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Groups</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <FeedIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Feed</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <CalendarMonthIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Events</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <BuildCircleIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Settings </span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <SportsEsportsIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Games</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <NewspaperIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">News </span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <FitnessCenterIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Jobs</span>
+                </li>
+                <li className="leftPaneMenuItem">
+                    <AddShoppingCartIcon className='LeftPageMenuIcon'/>
+                    <span className="leftPaneMenuText">Market</span>
+                </li>
+                <hr/>
+                
+                <div className="pagesYouLiked">
+                    <h3>Liked Pages</h3>
+                </div>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+                <li className="pageListItem">
+                    <img src="./Images/1.jpg" alt="" className="pagePic" />
+                    <span className="pageName">Sharuka - Software Engineer</span>
+                </li>
+
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import NavBar from '../Components/Navigation/NavBar'
+import LeftPane from '../Components/LeftPage/LeftPane'
+import RightPane from '../Components/RightPane/RightPane'
+import PostPane from '../Components/PostPane/PostPane'
+import "./home.css"
 
 export default function Home
 () {
   return (
     <div>
        
-       <NavBar/>
+      <NavBar/>
+      
+        <div className="bottomContainer">
+          <LeftPane/>
+          <PostPane/>
+          <RightPane/>
+        </div>
 
+    
 
     </div>
 
